70c7309a083ed309115d313aa42c288e565ca277
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-03-30 03:15:47 +0000

    add login screen dart

---
+++ lib/features/login/login_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:weathershare/constants/image_strings.dart';
+import 'package:weathershare/constants/sizes.dart';
+import 'package:weathershare/constants/text_string.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(defaultSize),
+          child: Column(
+            children: [
+              Image(image: AssetImage(welcomeScreenImage)),
+              Text(
+                loginTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                loginSubTitle,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
