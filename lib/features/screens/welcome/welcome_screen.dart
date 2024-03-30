41079afded51926f7be3f8d1bb9cf6833174e5a5
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-03-30 15:33:56 +0000

    fix button color

--- lib/features/screens/welcome/welcome_screen.dart
+++ lib/features/screens/welcome/welcome_screen.dart
@@ -11,19 +11,23 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(welcomeScreenImage),
-            const SizedBox(height: 15),
+            Image.asset(
+              welcomeScreenImage,
+              height: height * 0.5,
+            ),
+            const SizedBox(height: 10),
             Text(
               loginTitle,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            const SizedBox(height: 15),
+            const SizedBox(height: 10),
             Text(
               loginSubTitle,
               style: Theme.of(context).textTheme.titleSmall,
@@ -36,7 +40,7 @@ class WelcomeScreen extends StatelessWidget {
         child: Container(
           color: Colors.white,
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 90.0, horizontal: 16),
+            padding: const EdgeInsets.symmetric(vertical: 90.0, horizontal: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -45,18 +49,23 @@ class WelcomeScreen extends StatelessWidget {
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
                       minimumSize: const Size(100, 50),
+                      foregroundColor: whiteColor,
+                      backgroundColor: secondaryColor,
+                      side: const BorderSide(color: secondaryColor),
                     ),
-                    child: const Text(login),
+                    child: Text(login.toUpperCase()),
                   ),
                 ),
-                const SizedBox(width: 20),
+                const SizedBox(width: 16),
                 Expanded(
                   child: OutlinedButton(
                     onPressed: () {},
                     style: OutlinedButton.styleFrom(
                       minimumSize: const Size(150, 50),
+                      foregroundColor: secondaryColor,
+                      side: const BorderSide(color: secondaryColor),
                     ),
-                    child: const Text(signUp),
+                    child: Text(signUp.toUpperCase()),
                   ),
                 ),
               ],
