090ae097549e0a37758002af759578c3eb053924
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-05 11:21:46 +0300

    extract login footer widget

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:weathershare/constants/image_strings.dart';
 import 'package:weathershare/constants/sizes.dart';
-import 'package:weathershare/constants/text_string.dart';
 import 'package:weathershare/features/login/login_form_widget.dart';
 import 'package:weathershare/features/login/login_header_widget.dart';
+import 'package:weathershare/features/login/login_footer_widget.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -21,29 +19,7 @@ class LoginScreen extends StatelessWidget {
             children: [
               LoginHeaderWidget(size: size),
               const LoginForm(),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text("OR"),
-                  const SizedBox(height: formHeight - 16),
-                  SizedBox(
-                    width: double.infinity,
-                    child: OutlinedButton.icon(
-                      onPressed: () {},
-                      label: const Text(signInWithGoogle),
-                      icon: const Image(
-                        image: AssetImage(googleLogo),
-                        width: 25.0,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: formHeight - 16),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text(alreadyHaveAnAccount),
-                  )
-                ],
-              )
+              const LoginFooterWidget()
             ],
           ),
         ),
