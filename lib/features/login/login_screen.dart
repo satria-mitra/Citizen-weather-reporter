e5775dcbe555e5e52224f4b3c521c22bd6b0a867
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-05 10:25:27 +0300

    extract login form to a widget

--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:weathershare/constants/image_strings.dart';
 import 'package:weathershare/constants/sizes.dart';
-import 'package:weathershare/constants/text_string.dart';
 import 'package:weathershare/features/login/login_form_widget.dart';
+import 'package:weathershare/features/login/login_header_widget.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -16,21 +15,7 @@ class LoginScreen extends StatelessWidget {
           padding: const EdgeInsets.all(defaultSize),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image(
-                image: const AssetImage(logInImage),
-                height: size.height * 0.3,
-              ),
-              Text(
-                loginTitle,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Text(
-                loginSubTitle,
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              const LoginForm()
-            ],
+            children: [LoginHeaderWidget(size: size), const LoginForm()],
           ),
         ),
       ),
