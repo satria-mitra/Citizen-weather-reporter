bd2fbc5620496e98711ae4c22c56acd482bd975d
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-05 20:29:49 +0300

    add header widget

---
+++ lib/common_widgets/form_header_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:weathershare/constants/image_strings.dart';
+import 'package:weathershare/constants/text_string.dart';
+
+class FormHeaderWidget extends StatelessWidget {
+  const FormHeaderWidget({
+    super.key,
+    required this.image,
+    required this.title,
+  });
+
+  final String image, title;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+          image: const AssetImage(signUpImage),
+          height: size.height * 0.2,
+        ),
+        // const SizedBox(height: formHeight - 16),
+
+        Text(
+          signUpTitle,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        // const SizedBox(height: formHeight - 16),
+        // Text(
+        //   loginSubTitle,
+        //   style: Theme.of(context).textTheme.headlineSmall,
+        // ),
+      ],
+    );
+  }
+}
