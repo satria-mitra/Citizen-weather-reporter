fa69384672ca3462cb691b9a8a5ca34c0ab43a03
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-05 09:52:36 +0300

    fix color lib

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,9 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF03A9F4);
-const secondaryColor = Color(0xFF03A9F4);
+const primaryColor = Color(0xffffffff);
+const secondaryColor = Color(0xFF272727);
 const accentColor = Color(0xFF03A9F4);
 
 const cardBgColor = Color(0xFFF7F6F1);
-const whiteColor = Color(0xffffffff);
+const whiteColor = Colors.white;
 const darkColor = Color(0xFF000000);
