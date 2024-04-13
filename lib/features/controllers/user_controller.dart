dd9bd0945426c259adaf09151eb206bfda60d54c
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-13 11:30:13 +0100

    add user controller

---
+++ lib/features/controllers/user_controller.dart
@@ -0,0 +1,83 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weathershare/features/models/user_model.dart';
+import 'package:weathershare/repository/user_repo.dart';
+
+class UserController extends GetxController {
+  static UserController get instance => Get.find();
+
+  Rx<UserModel> user = UserModel.empty().obs;
+  final imageUploading = false.obs;
+  final profileLoading = false.obs;
+  final profileImageUrl = ''.obs;
+  final hidePassword = false.obs;
+  final verifyEmail = TextEditingController();
+  final verifyPassword = TextEditingController();
+  final userRepository = Get.put(UserRepository());
+  GlobalKey<FormState> reAuthFormKey = GlobalKey<FormState>();
+
+  /// init user data when Home Screen appears
+  @override
+  void onInit() {
+    fetchUserRecord();
+    super.onInit();
+  }
+
+  // Fetch user record
+  Future<void> fetchUserRecord() async {
+    try {
+      profileLoading.value = true;
+      final user = await userRepository.fetchUserDetails();
+      this.user(user);
+    } catch (e) {
+      user(UserModel.empty());
+    } finally {
+      profileLoading.value = false;
+    }
+  }
+
+  // Save user Record from any Registration provider
+  Future<void> saveUserRecord(
+      {UserModel? user, UserCredential? userCredentials}) async {
+    try {
+      // First UPDATE Rx User and then check if user data is already stored. If not store new data
+      await fetchUserRecord();
+
+      // If no record already stored.
+      if (this.user.value.id.isEmpty) {
+        if (userCredentials != null) {
+          // Convert Name to First and Last Name
+          final nameParts =
+              UserModel.nameParts(userCredentials.user!.displayName ?? '');
+          final customUsername = UserModel.generateUsername(
+              userCredentials.user!.displayName ?? '');
+
+          // Map data
+          final newUser = UserModel(
+            id: userCredentials.user!.uid,
+            firstname: nameParts[0],
+            lastname:
+                nameParts.length > 1 ? nameParts.sublist(1).join(' ') : "",
+            email: userCredentials.user!.email ?? '',
+            profilePicture: userCredentials.user!.photoURL ?? '',
+          );
+
+          // Save user data
+          await userRepository.saveUserRecord(newUser);
+
+          // Assign new user to the RxUser so that we can use it through out the app.
+          this.user(newUser);
+        } else if (user != null) {
+          // Save Model when user registers using Email and Password
+          await userRepository.saveUserRecord(user);
+
+          // Assign new user to the RxUser so that we can use it through out the app.
+          this.user(user);
+        }
+      }
+    } catch (e) {
+      //print('Something went wrong while saving your information');
+    }
+  }
+}
