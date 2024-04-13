67afae4eb658b3496c769f373c09c9374af91f4d
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-13 07:50:52 +0100

    add more code from another login widget

--- lib/features/screens/login/login_screen.dart
+++ lib/features/screens/login/login_screen.dart
@@ -110,3 +110,46 @@ class LoginForm extends StatelessWidget {
     ));
   }
 }
+
+class LoginFooterWidget extends StatelessWidget {
+  const LoginFooterWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text("OR"),
+        const SizedBox(height: formHeight - 16),
+        SizedBox(
+          width: double.infinity,
+          child: OutlinedButton.icon(
+            onPressed: () {},
+            label: const Text(signInWithGoogle),
+            icon: const Image(
+              image: AssetImage(googleLogo),
+              width: 24.0,
+            ),
+          ),
+        ),
+        const SizedBox(height: formHeight - 16),
+        TextButton(
+          onPressed: () {},
+          child: Text.rich(
+            TextSpan(
+                text: dontHaveAccount,
+                style: Theme.of(context).textTheme.bodyMedium,
+                children: const [
+                  TextSpan(
+                    text: signUp,
+                    style: TextStyle(color: Colors.lightBlue),
+                  )
+                ]),
+          ),
+        ),
+      ],
+    );
+  }
+}
