089e98d35f1fbba17465e6a3564e05ca3749906a
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-13 07:55:31 +0100

    add more codes from another pages

--- lib/features/screens/on_boarding/on_boarding_view.dart
+++ lib/features/screens/on_boarding/on_boarding_view.dart
@@ -2,15 +2,41 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:weathershare/constants/text_string.dart';
-import 'package:weathershare/features/screens/on_boarding/on_boarding_item.dart';
 import 'package:weathershare/features/screens/welcome/welcome_screen.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:weathershare/constants/image_strings.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
 
   @override
   State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class OnboardingInfo {
+  final String title;
+  final String description;
+  final String image;
+
+  OnboardingInfo(
+      {required this.title, required this.description, required this.image});
+}
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        title: onBoardingTitle1,
+        description: onBoardingSubTitle1,
+        image: onBoardingImages1),
+    OnboardingInfo(
+        title: onBoardingTitle2,
+        description: onBoardingSubTitle2,
+        image: onBoardingImages2),
+    OnboardingInfo(
+        title: onBoardingTitle3,
+        description: onBoardingSubTitle3,
+        image: onBoardingImages3),
+  ];
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
@@ -101,10 +127,11 @@ class _OnBoardingViewState extends State<OnBoardingView> {
           final storage = GetStorage();
 
           if (kDebugMode) {
-            print("-- get storage --");
-            print(storage.read('IsFirstTime'));
+            print("--- get storage ---");
           }
           storage.write('IsFirstTime', false);
+          print(storage.read('IsFirstTime'));
+
           //print("Setting onboarding to true"); // Debug line
 
           if (!mounted) return;
