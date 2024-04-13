05b4adfb13747ed6f579e641bd0cd6c1d9f374af
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-13 07:48:34 +0100

    combine to one login screen

--- lib/features/screens/login/login_screen.dart
+++ lib/features/screens/login/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:weathershare/constants/sizes.dart';
-import 'package:weathershare/features/screens/login/login_form_widget.dart';
-import 'package:weathershare/features/screens/login/login_header_widget.dart';
 import 'package:weathershare/features/screens/login/login_footer_widget.dart';
+import 'package:weathershare/constants/text_string.dart';
+import 'package:weathershare/constants/image_strings.dart';
+import 'package:get/get.dart';
+import 'package:weathershare/features/screens/forget_password/forget_password.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -20,5 +22,91 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class LoginHeaderWidget extends StatelessWidget {
+  const LoginHeaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 40),
+        Center(
+          child: Image(
+            image: const AssetImage(logInImage),
+            height: size.height * 0.3,
+          ),
+        ),
+        const SizedBox(height: formHeight - 16),
+
+        Text(
+          loginTitle,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        const SizedBox(height: formHeight - 16),
+
+        // Text(
+        //   loginSubTitle,
+        //   style: Theme.of(context).textTheme.headlineSmall,
+        // ),
+      ],
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextFormField(
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.person_outline_outlined),
+              labelText: email,
+              hintText: email,
+              border: OutlineInputBorder()),
+        ),
+        const SizedBox(height: formHeight - 10),
+        TextFormField(
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.fingerprint),
+            labelText: password,
+            hintText: password,
+            border: OutlineInputBorder(),
+            suffixIcon: IconButton(
+              onPressed: null,
+              icon: Icon(Icons.remove_red_eye_sharp),
+            ),
+          ),
+        ),
+        const SizedBox(height: formHeight - 16),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: () => Get.to(() => const ForgetPasswordMail()),
+            child: const Text(forgetPassword),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(login.toUpperCase()),
+          ),
+        ),
+        const SizedBox(height: formHeight - 16),
+      ],
+    ));
   }
 }
