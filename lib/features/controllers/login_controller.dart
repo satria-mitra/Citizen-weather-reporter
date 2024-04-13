232096ef9bb3a9d61ee36b14953800f966aa8205
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-13 11:30:35 +0100

    add login controller

--- lib/features/controllers/login_controller.dart
+++ lib/features/controllers/login_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:weathershare/features/controllers/user_controller.dart';
+import 'package:weathershare/repository/auth_repo.dart';
 
 class LoginController extends GetxController {
   static LoginController get instance => Get.find();
@@ -8,8 +10,24 @@ class LoginController extends GetxController {
   // Variables
   final hidePassword = true.obs;
   final rememberMe = false.obs;
-
+  final localStorage = GetStorage();
   final email = TextEditingController();
   final password = TextEditingController();
   GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+
+  @override
+  void onInit() {
+    email.text = localStorage.read('REMEMBER_ME_EMAIL') ?? '';
+    password.text = localStorage.read('REMEMBER_ME_PASSWORD') ?? '';
+    super.onInit();
+  }
+
+  //Sign in with email and password
+  Future<void> emailAndPasswordSignIn() async {
+    final userCredentials = await AuthenticationRepo.instance
+        .loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+    await UserController.instance.fetchUserRecord();
+    await AuthenticationRepo.instance.screenRedirect(userCredentials.user);
+  }
 }
