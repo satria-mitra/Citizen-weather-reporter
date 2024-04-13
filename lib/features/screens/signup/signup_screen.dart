4a37f6cd22978c63943649d0786b098ec73f7caf
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-13 07:59:25 +0100

    add more code from sign up widget

--- lib/features/screens/signup/signup_screen.dart
+++ lib/features/screens/signup/signup_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:weathershare/common_widgets/form_header_widget.dart';
 import 'package:weathershare/constants/sizes.dart';
 import 'package:weathershare/constants/image_strings.dart';
-import 'package:weathershare/features/screens/signup/signup_form_widget.dart';
 import 'package:weathershare/constants/text_string.dart';
+import 'package:weathershare/features/controllers/signup_controller.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -51,6 +52,80 @@ class SignUpScreen extends StatelessWidget {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpFormWidget extends StatelessWidget {
+  const SignUpFormWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(SignUpController());
+
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: formHeight - 16),
+      child: Form(
+        key: controller.signupFormKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: controller.firstName,
+              decoration: const InputDecoration(
+                label: Text(firstName),
+                prefixIcon: Icon(Icons.person_outline_rounded),
+              ),
+              // Add more FormFields or widgets here as needed
+            ),
+            const SizedBox(height: formHeight - 24),
+            TextFormField(
+              controller: controller.lastName,
+              decoration: const InputDecoration(
+                label: Text(lastName),
+                prefixIcon: Icon(Icons.person_2_outlined),
+              ),
+              // Add more FormFields or widgets here as needed
+            ),
+            const SizedBox(height: formHeight - 24),
+            TextFormField(
+              controller: controller.email,
+              decoration: const InputDecoration(
+                label: Text(email),
+                prefixIcon: Icon(Icons.email_outlined),
+              ),
+              // Add more FormFields or widgets here as needed
+            ),
+            const SizedBox(height: formHeight - 24),
+            Obx(
+              () => TextFormField(
+                controller: controller.password,
+                obscureText: controller.hidePassword.value,
+                decoration: InputDecoration(
+                  label: const Text(password),
+                  prefixIcon: const Icon(Icons.fingerprint),
+                  suffixIcon: IconButton(
+                    onPressed: () => controller.hidePassword.value =
+                        !controller.hidePassword.value,
+                    icon: const Icon(Icons.visibility_outlined),
+                  ),
+                  // Add more FormFields or widgets here as needed
+                ),
+              ),
+            ),
+            const SizedBox(height: formHeight - 16),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => controller.signup(),
+                child: Text(signUp.toUpperCase()),
+              ),
+            )
+          ],
         ),
       ),
     );
