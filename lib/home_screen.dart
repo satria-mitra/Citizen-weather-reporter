79bc76775292556c33eb1bed7f21622d06d68890
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-14 03:58:23 +0100

    update home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,23 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeMenu extends StatelessWidget {
   const HomeMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+
     return Scaffold(
-        bottomNavigationBar: NavigationBar(
-      destinations: const [
-        NavigationDestination(icon: Icon(Icons.map_outlined), label: 'Home'),
-        NavigationDestination(
-            icon: Icon(Icons.analytics_outlined), label: 'Home'),
-        NavigationDestination(
-            icon: Icon(Icons.add_circle_outline), label: 'Home'),
-        NavigationDestination(
-            icon: Icon(Icons.add_chart_outlined), label: 'Home'),
-        NavigationDestination(
-            icon: Icon(Icons.account_circle_outlined), label: 'Home'),
-      ],
-    ));
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedMenu.value,
+          onDestinationSelected: (index) =>
+              controller.selectedMenu.value = index,
+          destinations: const [
+            NavigationDestination(
+                icon: Icon(Icons.map_outlined), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.analytics_outlined), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.add_circle_outline), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.add_chart_outlined), label: 'Home'),
+            NavigationDestination(
+                icon: Icon(Icons.account_circle_outlined), label: 'Home'),
+          ],
+        ),
+      ),
+      body: Container(),
+    );
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedMenu = 0.obs;
+  final screens = [
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.black,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.yellow,
+    )
+  ];
 }
