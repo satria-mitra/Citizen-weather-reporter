fb9f7883e9677a0826543cb0a51d6e7dea22c0e4
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-15 04:09:14 +0100

    add photo to user profile screen

--- lib/features/screens/user_profile/user_screen.dart
+++ lib/features/screens/user_profile/user_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weathershare/constants/image_strings.dart';
+import 'package:weathershare/constants/sizes.dart';
+import 'package:weathershare/constants/text_string.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -11,6 +14,32 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     var isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // No back button
+
+        title: Text(
+          userProfile,
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(defaultSize),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: const Image(image: AssetImage(userProfilePicture)),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
