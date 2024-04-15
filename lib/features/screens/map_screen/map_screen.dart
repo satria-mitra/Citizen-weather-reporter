94883a0ea11035dabb8106192902f174e7e9b9d3
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-15 03:20:39 +0100

    try to add more marker with different color

--- lib/features/screens/map_screen/map_screen.dart
+++ lib/features/screens/map_screen/map_screen.dart
@@ -9,8 +9,43 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late GoogleMapController mapController;
+  late GoogleMapController mapController; // Controller for Google Map
   final LatLng _center = const LatLng(51.540992, -0.015719); // Initial location
+
+  // Marker Set
+  final Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize markers
+    _markers.add(
+      Marker(
+        markerId: MarkerId('mainMarker'),
+        position: _center, // Marker position
+        infoWindow: InfoWindow(
+          // Popup info
+          title: 'Marker in London',
+          snippet: 'This is a snippet',
+        ),
+        icon: BitmapDescriptor.defaultMarker, // Default icon (red)
+      ),
+    );
+
+    // Add another marker with a different color
+    _markers.add(
+      Marker(
+        markerId: MarkerId('secondMarker'),
+        position: LatLng(51.540288, -0.015204), // Slightly different location
+        infoWindow: InfoWindow(
+          title: 'Second Marker',
+          snippet: 'Different color marker',
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueBlue), // Blue color marker
+      ),
+    );
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -43,7 +78,7 @@ class _MapScreenState extends State<MapScreen> {
             Column(
               children: [
                 SizedBox(
-                  height: 300, // Define the height of the map area
+                  height: 450, // Define the height of the map area
                   child: GoogleMap(
                     onMapCreated: _onMapCreated,
                     initialCameraPosition: CameraPosition(
@@ -51,15 +86,12 @@ class _MapScreenState extends State<MapScreen> {
                       zoom: 15.0,
                     ),
                     mapType: MapType.normal,
-                    zoomGesturesEnabled: true, // Enable zoom gestures
-                    scrollGesturesEnabled:
-                        true, // Enable scroll gestures (panning)
-                    rotateGesturesEnabled: true, // Optional: Allow map rotation
-                    tiltGesturesEnabled:
-                        true, // Enable scroll gestures (panning)
+                    zoomGesturesEnabled: true,
+                    scrollGesturesEnabled: true,
+                    markers:
+                        _markers, // Assign marker set to the GoogleMap widget
                   ),
                 ),
-                // More widgets can be added here if needed
               ],
             )
           ],
