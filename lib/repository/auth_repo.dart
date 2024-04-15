83e3c5aef64dfe629cdebe7e65a23e6c9b9e0250
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-15 16:01:59 +0100

    fix redirect screen after login

--- lib/repository/auth_repo.dart
+++ lib/repository/auth_repo.dart
@@ -6,7 +6,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:weathershare/features/screens/login/login_screen.dart';
 import 'package:weathershare/features/screens/on_boarding/on_boarding_view.dart';
 import 'package:weathershare/features/screens/welcome/welcome_screen.dart';
-import 'package:weathershare/screens/homescreen.dart';
+import 'package:weathershare/home_screen.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:weathershare/utils/storage/storage.dart';
 //import 'package:weathershare/utils/storage/storage.dart';
@@ -50,7 +50,7 @@ class AuthenticationRepo extends GetxController {
     if (user != null) {
       // User is logged in
       await LocalStorage.init(user.uid);
-      Get.offAll(() => const HomeScreen());
+      Get.offAll(() => const HomeMenu());
     } else {
       Get.offAll(() => const WelcomeScreen());
     }
@@ -153,5 +153,21 @@ class AuthenticationRepo extends GetxController {
     }
   }
 
-  Future<void> logout() async => await _auth.signOut();
+  Future<void> logout() async {
+    try {
+      await GoogleSignIn().signOut();
+      await FirebaseAuth.instance.signOut();
+      Get.offAll(() => const LoginScreen());
+    } on FirebaseAuthException catch (e) {
+      throw 'Something wrong with Authentication. Please try again';
+    } on FirebaseException catch (e) {
+      throw 'An unknown Firebase error occurred. Please try again';
+    } on FormatException catch (_) {
+      throw 'The email address format is invalid. Please enter a valid email.';
+    } on PlatformException catch (e) {
+      throw 'An unexpected platform error occurred. Please try again.';
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
 }
