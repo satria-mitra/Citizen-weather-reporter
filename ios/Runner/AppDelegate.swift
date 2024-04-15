cbb3cd3b2fe2a0bdea278a0dfaa1ab2873d24078
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-15 02:55:05 +0100

    rebuild ios folder

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,8 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+            GMSServices.provideAPIKey("AIzaSyDPzDT2dwuqS8KnrUCt8jXwKtqVjsj3uB0")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
