19f791fca4bf356cf6a7a729a0d763ca2280f136
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-20 12:15:23 +0100

    update add iot devices screen

--- lib/features/screens/add_iot_devices/add_iot_devices_screen.dart
+++ lib/features/screens/add_iot_devices/add_iot_devices_screen.dart
@@ -3,24 +3,52 @@ import 'package:get/get.dart';
 import 'package:weathershare/constants/colors.dart';
 import 'package:weathershare/constants/sizes.dart';
 import 'package:weathershare/constants/text_string.dart';
+import 'package:weathershare/features/controllers/add_broker_controller.dart';
 import 'package:weathershare/features/controllers/add_iot_devices_controller.dart';
 
 class AddIoTDevicesScreen extends StatefulWidget {
-  const AddIoTDevicesScreen({super.key});
+  final String initialBroker;
+  const AddIoTDevicesScreen({super.key, required this.initialBroker});
 
   @override
   State<AddIoTDevicesScreen> createState() => _AddIoTDevicesScreenState();
 }
 
 class _AddIoTDevicesScreenState extends State<AddIoTDevicesScreen> {
+  late String selectedBroker;
+  List<String> brokers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    selectedBroker =
+        widget.initialBroker; // Set initial broker from widget parameter
+    brokers = [selectedBroker]; // Start with the initial broker
+    loadAdditionalBrokers(); // Then load additional brokers
+  }
+
+  void loadAdditionalBrokers() async {
+    final controller = Get.put(IoTDevicesController());
+
+    var fetchedBrokers = await controller.fetchBrokerNames();
+    print("Fetched brokers: $fetchedBrokers"); // Debug output
+
+    setState(() {
+      brokers.addAll(fetchedBrokers
+          .where((br) => br != widget.initialBroker && br != "Unnamed Broker")
+          .toList());
+      print("Brokers for dropdown: $brokers"); // Check final list
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(IoTDevicesController());
+
     var isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Scaffold(
       backgroundColor: isDark ? secondaryColor : primaryColor,
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         title: Text(
           "Add IoT Device",
           style: Theme.of(context).textTheme.headlineMedium,
@@ -32,66 +60,25 @@ class _AddIoTDevicesScreenState extends State<AddIoTDevicesScreen> {
           key: controller.iotDeviceFormKey,
           children: [
             Text(
-              "Add MQTT Broker Details",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: controller.mqttHost,
-              decoration: const InputDecoration(
-                labelText: "MQTT Broker Host",
-                hintText: "mqtt.broker.org",
-                prefixIcon: Icon(Icons.link),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: controller.mqttPort,
-              decoration: const InputDecoration(
-                labelText: "Port",
-                hintText: "1883 / 1884",
-                prefixIcon: Icon(Icons.devices),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: controller.mqttUsername,
-              decoration: const InputDecoration(
-                labelText: "Username",
-                prefixIcon: Icon(Icons.account_tree_outlined),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Obx(
-              () => TextFormField(
-                obscureText: controller.hidePassword.value,
-                controller: controller.mqttPassword,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.fingerprint),
-                  labelText: password,
-                  hintText: password,
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    onPressed: () => controller.hidePassword.value =
-                        !controller.hidePassword.value,
-                    icon: const Icon(Icons.remove_red_eye_sharp),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: controller.mqttTopic,
-              decoration: const InputDecoration(
-                labelText: "Topic",
-                hintText: "Topic to parameter",
-                prefixIcon: Icon(Icons.topic),
-              ),
-            ),
-            const SizedBox(height: 24),
-            Text(
               "Add Devices Metadata",
               style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 8),
+            DropdownButtonFormField<String>(
+              value: selectedBroker,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedBroker = newValue ?? selectedBroker;
+                  print(
+                      "Selected Broker changed to: $selectedBroker"); // Debug print
+                });
+              },
+              items: brokers.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
             const SizedBox(height: 8),
             TextFormField(
@@ -103,9 +90,21 @@ class _AddIoTDevicesScreenState extends State<AddIoTDevicesScreen> {
             ),
             const SizedBox(height: 8),
             TextFormField(
+              controller: controller.mqttTopic,
+              decoration: const InputDecoration(
+                labelText: "Topic to subscribe",
+                hintText: "e.g /Topic",
+                prefixIcon: Icon(
+                  Icons.topic_outlined,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextFormField(
               controller: controller.deviceId,
               decoration: const InputDecoration(
                 labelText: "Device ID",
+                hintText: "Unique device ID",
                 prefixIcon: Icon(Icons.perm_identity_outlined),
               ),
             ),
@@ -118,7 +117,7 @@ class _AddIoTDevicesScreenState extends State<AddIoTDevicesScreen> {
                     controller: controller.deviceLat,
                     decoration: const InputDecoration(
                       labelText: "Latitude",
-                      hintText: "Like 51.5388345",
+                      hintText: "e.g 51.5388345",
                       prefixIcon: Icon(Icons.location_on_outlined),
                     ),
                   ),
@@ -131,7 +130,7 @@ class _AddIoTDevicesScreenState extends State<AddIoTDevicesScreen> {
                     controller: controller.deviceLong,
                     decoration: const InputDecoration(
                       labelText: "Longitude",
-                      hintText: "Like -0.0106886",
+                      hintText: "e.g -0.0106886",
                       prefixIcon: Icon(Icons.location_on_outlined),
                     ),
                   ),
@@ -142,7 +141,7 @@ class _AddIoTDevicesScreenState extends State<AddIoTDevicesScreen> {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () => controller.createNewBrokerDetails(),
+                onPressed: () => controller.saveIoTDevicesDetails(),
                 child: Text("Add Device".toUpperCase()),
               ),
             ),
