ffd61d259f07dc9da4b100f81435089eed42e7b0
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-20 04:12:07 +0100

    add save broker function

--- lib/features/controllers/add_broker_controller.dart
+++ lib/features/controllers/add_broker_controller.dart
@@ -20,23 +20,46 @@ class AddBrokersController extends GetxController {
   Future<List<String>> fetchBrokerNames() async {
     try {
       QuerySnapshot snapshot = await _db.collection('mqttBrokers').get();
-      List<String> brokerNames = snapshot.docs
-          .map((doc) => doc.data().containsKey('name')
-              ? doc.data()['name'] as String
-              : 'Unnamed Broker') // Checks if 'name' exists and is not null
-          .toList();
+      List<String> brokerNames = snapshot.docs.map((doc) {
+        var data =
+            doc.data() as Map<String, dynamic>?; // Cast to a nullable Map
+        if (data != null && data.containsKey('name')) {
+          return data['name'] as String; // Safely return the name if available
+        } else {
+          return 'Unnamed Broker'; // Default name if 'name' key is missing or data is null
+        }
+      }).toList();
       if (brokerNames.isEmpty) {
-        // Ensures there's always a non-null return
         return [
           'No Brokers Found'
-        ]; // You might want to handle this case differently in your UI
+        ]; // Return this if no brokers are available at all
       }
-      return brokerNames;
+      return brokerNames; // Return the list of names
     } catch (e) {
       print("Failed to fetch broker names: $e");
-      return ['Fetch Failed']; // Provides a default value in case of failure
+      return ['Fetch Failed']; // Return this in case of any exceptions
     }
   }
 
-  // Other methods...
+  // Saves new broker details to Firestore
+  Future<void> saveBrokerDetails() async {
+    if (brokersFormKey.currentState?.validate() ?? false) {
+      // Ensures form is valid
+      try {
+        await _db.collection('brokers').add({
+          'host': mqttHost.text
+              .trim(), // Using trim to remove any leading/trailing whitespace
+          'port': mqttPort.text.trim(),
+          'username': mqttUsername.text.trim(),
+          'password': mqttPassword.text.trim(),
+        });
+        Get.snackbar('Success', 'Broker saved successfully!',
+            backgroundColor: Colors.green, snackPosition: SnackPosition.BOTTOM);
+      } catch (e) {
+        Get.snackbar('Error', 'Failed to save broker: $e',
+            backgroundColor: Colors.red, snackPosition: SnackPosition.BOTTOM);
+        print("Failed to save broker details: $e");
+      }
+    }
+  }
 }
