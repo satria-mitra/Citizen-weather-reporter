ff50e4ebc3b43d137f4eb56311e5967abe767469
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-20 12:14:48 +0100

    add saveiotdevices function

--- lib/features/controllers/add_iot_devices_controller.dart
+++ lib/features/controllers/add_iot_devices_controller.dart
@@ -6,7 +6,10 @@ class IoTDevicesController extends GetxController {
   static IoTDevicesController get instance => Get.find();
 
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final RxString selectedBroker = ''.obs; // Reactive selected broker
+
   final hidePassword = true.obs;
+  final mqttName = TextEditingController();
   final mqttHost = TextEditingController();
   final mqttPort = TextEditingController();
   final mqttUsername = TextEditingController();
@@ -16,42 +19,76 @@ class IoTDevicesController extends GetxController {
   final deviceId = TextEditingController();
   final deviceLong = TextEditingController();
   final deviceLat = TextEditingController();
+  final mqttBrokerName = TextEditingController();
+
   GlobalKey<FormState> iotDeviceFormKey = GlobalKey<FormState>();
 
-  Future<void> createNewBrokerDetails() async {
-    QuerySnapshot existingBroker = await _db
-        .collection('mqttBrokers')
-        .where('deviceID', isEqualTo: deviceId.text)
-        .get();
-
-    if (existingBroker.docs.isNotEmpty) {
-      // If there is an existing broker with the same deviceID, show a snackbar and return
-      Get.snackbar(
-          'Broker Already Added', 'This broker has already been added.',
-          backgroundColor: Colors.amber, snackPosition: SnackPosition.BOTTOM);
-      return; // Stop the function here if broker already exists
-    }
+  // Fetch broker names from Firestore
+  Future<List<String>> fetchBrokerNames() async {
     try {
-      await _db.collection('mqttBrokers').add({
-        'host': mqttHost.text,
-        'port': mqttPort.text,
-        'username': mqttUsername.text,
-        'password': mqttPassword.text,
-        'topic': mqttTopic.text,
-        'deviceName': deviceName.text,
-        'deviceID': deviceId.text,
-        'location': {
-          'latitude': double.tryParse(deviceLat.text) ?? 0.0,
-          'longitude': double.tryParse(deviceLong.text) ?? 0.0
-        },
-      });
-      Get.snackbar('Success', 'Broker details added successfully!',
-          backgroundColor: Colors.green, snackPosition: SnackPosition.BOTTOM);
-    } catch (e, stackTrace) {
-      print('Error adding broker details: $e');
-      print('Stack trace: $stackTrace');
-      Get.snackbar('Error', 'Failed to add broker details: $e',
-          backgroundColor: Colors.red, snackPosition: SnackPosition.BOTTOM);
+      QuerySnapshot snapshot = await _db.collection('brokers').get();
+      List<String> brokerNames = snapshot.docs.map((doc) => doc.id).toList();
+      if (brokerNames.isEmpty) {
+        brokerNames.add('No Brokers Found');
+      }
+      return brokerNames;
+    } catch (e) {
+      print("Failed to fetch broker names: $e");
+      return ['Fetch Failed'];
+    }
+  }
+
+  // Saves new broker details to Firestore
+  Future<void> saveBrokerDetails() async {
+    if (iotDeviceFormKey.currentState?.validate() ?? false) {
+      String brokerId = mqttName.text
+          .trim()
+          .replaceAll(' ', '_'); // Replace spaces with underscores
+      try {
+        await _db.collection('brokers').doc(brokerId).set({
+          'mqttname': mqttName.text.trim(),
+          'host': mqttHost.text.trim(),
+          'port': mqttPort.text.trim(),
+          'username': mqttUsername.text.trim(),
+          'password': mqttPassword.text.trim(),
+        });
+        Get.snackbar('Success', 'Broker saved successfully',
+            backgroundColor: Colors.green, snackPosition: SnackPosition.BOTTOM);
+        selectedBroker.value = brokerId; // Update selected broker
+      } catch (e) {
+        Get.snackbar('Error', 'Failed to save broker: $e',
+            backgroundColor: Colors.red, snackPosition: SnackPosition.BOTTOM);
+        print("Failed to save broker details: $e");
+      }
+    }
+  }
+
+  // Method to save IoT device details
+  Future<void> saveIoTDevicesDetails() async {
+    if (iotDeviceFormKey.currentState?.validate() ?? false) {
+      try {
+        DocumentReference deviceRef =
+            _db.collection('devices').doc(deviceId.text);
+        await deviceRef.set({
+          'topic': mqttTopic.text.trim(),
+          'deviceName': deviceName.text.trim(),
+          'deviceID': deviceId.text.trim(),
+          'location': {
+            'latitude': double.tryParse(deviceLat.text) ?? 0.0,
+            'longitude': double.tryParse(deviceLong.text) ?? 0.0
+          },
+          'brokerRef': _db
+              .collection('brokers')
+              .doc(selectedBroker.value) // Use selected broker
+        });
+        Get.snackbar('Success', 'Device details added successfully!',
+            backgroundColor: Colors.green, snackPosition: SnackPosition.BOTTOM);
+      } catch (e, stackTrace) {
+        print('Error adding device details: $e');
+        print('Stack trace: $stackTrace');
+        Get.snackbar('Error', 'Failed to add device details: $e',
+            backgroundColor: Colors.red, snackPosition: SnackPosition.BOTTOM);
+      }
     }
   }
 }
