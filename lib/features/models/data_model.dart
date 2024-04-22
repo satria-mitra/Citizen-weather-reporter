fb2177465ca44e28ebc9129ed6edf656e1278cad
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-22 17:23:34 +0100

    add data model

---
+++ lib/features/models/data_model.dart
@@ -0,0 +1,7 @@
+class DeviceData {
+  final String time; // hh-mm-ss
+  final String tempAvg; // ta_avg
+  final String rhAvg; // rh_avg
+
+  DeviceData({required this.time, required this.tempAvg, required this.rhAvg});
+}
