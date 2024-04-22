e44fc1ae1a38e3ece11c24efe8e24e62c652384c
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-22 18:12:31 +0100

    add device model

---
+++ lib/features/models/device_model.dart
@@ -0,0 +1,16 @@
+class Device {
+  final String id;
+  final String name;
+  final String date;
+  final String time;
+  final String temperature;
+  final String humidity;
+
+  Device(
+      {required this.id,
+      required this.name,
+      required this.date,
+      required this.time,
+      required this.temperature,
+      required this.humidity});
+}
