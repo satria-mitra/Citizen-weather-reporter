34ad3cbced20b86327bf89d9aabf32884c192465
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-22 20:01:11 +0100

    remove unnecessary page

--- lib/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xffffffff);
-const secondaryColor = Color(0xFF272727);
-const accentColor = Color(0xFF03A9F4);
-
-const cardBgColor = Color(0xFFF7F6F1);
-const whiteColor = Colors.white;
-const darkColor = Color(0xFF000000);
--- lib/constants/sizes.dart
+++
@@ -1,4 +0,0 @@
-const defaultSize = 30.0;
-const splashContainerSize = 30.0;
-const buttonHeight = 15.0;
-const formHeight = 32.0;
