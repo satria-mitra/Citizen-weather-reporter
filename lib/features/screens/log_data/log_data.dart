e4a0c0fd04553655939fad1dfe61e1ce2c198d90
Satria Mitra Utama <satriamitrautama@gmail.com>
2024-04-22 14:57:38 +0100

    add date picker

--- lib/features/screens/log_data/log_data.dart
+++ lib/features/screens/log_data/log_data.dart
@@ -13,6 +13,7 @@ class LogDataScreen extends StatefulWidget {
 class _LogDataScreenState extends State<LogDataScreen> {
   String? selectedDevice;
   List<String> devices = [];
+  DateTime? selectedDate; // To store the selected date
 
   @override
   void initState() {
@@ -34,6 +35,20 @@ class _LogDataScreenState extends State<LogDataScreen> {
         devices = fetchedDevices;
       });
     });
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2024, 4, 21),
+      lastDate: DateTime(2024, 4, 23),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 
   @override
@@ -72,6 +87,15 @@ class _LogDataScreenState extends State<LogDataScreen> {
                     isExpanded: true,
                   ),
                 ),
+                SizedBox(
+                    width:
+                        20), // Spacing between the dropdown and the date picker
+                OutlinedButton(
+                  onPressed: () => _selectDate(context),
+                  child: Text(selectedDate == null
+                      ? 'Pick a Date'
+                      : '${selectedDate!.toIso8601String().substring(0, 10)}'),
+                ),
               ],
             ),
           ),
@@ -83,8 +107,14 @@ class _LogDataScreenState extends State<LogDataScreen> {
           else
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: const Text('Please select a device.'),
-            )
+              child: Text('Please select a device.'),
+            ),
+          if (selectedDate != null)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  'Selected Date: ${selectedDate!.toIso8601String().substring(0, 10)}'),
+            ),
         ],
       ),
     );
